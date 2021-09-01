5c4281a4d274e0eff8629513f0054c1c9ccd4dcb
Hernan Alex Choque Condori <hernanalexchoquecondori161@gmail.com>
2021-09-01 10:50:26 -0300

    add changes js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const listItem = document.querySelectorAll(".list-questions__item");
 const listText = document.querySelectorAll(".list-questions__container-text");
 const listIcon = document.querySelectorAll(".list-questions__icon");
+const listTitle = document.querySelectorAll(".list-questions__tittle");
 
 class CheckItem{
     constructor() { this.lastItem = -1; }
@@ -10,36 +11,38 @@ class CheckItem{
 
 let checkOut = new CheckItem();
 
-function addAnimation(objText, objIcon) {
+function addAnimation(objText, objIcon, objTittle) {
     objText.classList.toggle("list-questions__container-text--show");
     objIcon.classList.toggle("list-questions__icon--rotate");
+    objTittle.classList.toggle("list-questions__tittle--click-down");
 }
 
-function animation(objEvent, objText, objIcon, objCheckOut, index){
+function animation(objEvent, objText, objIcon, objTittle, objCheckOut, index){
     objEvent.addEventListener('click', function (event) {
         if(objCheckOut.getLastItem() != -1){
             if(objCheckOut.getLastItem() != index){
-                addAnimation(objText,objIcon);
+                addAnimation(objText,objIcon,objTittle);
                 
                 let elemText = document.querySelectorAll(".list-questions__container-text");
                 let elemIcon = document.querySelectorAll(".list-questions__icon");
+                let elemTittle = document.querySelectorAll(".list-questions__tittle");
 
-                addAnimation(elemText[objCheckOut.getLastItem()],elemIcon[objCheckOut.getLastItem()]);
+                addAnimation(elemText[objCheckOut.getLastItem()],elemIcon[objCheckOut.getLastItem()],elemTittle[objCheckOut.getLastItem()]);
                 objCheckOut.setLastItem(index);
             }
             else{
-                addAnimation(objText,objIcon);
+                addAnimation(objText,objIcon,objTittle);
                 objCheckOut.setLastItem(-1);
             }
         }
         else{
-            addAnimation(objText,objIcon);
+            addAnimation(objText,objIcon,objTittle);
             objCheckOut.setLastItem(index);
         }
     });
 }
 
 for(a = 0; a < listItem.length; a++){
-    animation(listItem[a], listText[a], listIcon[a], checkOut, a);
+    animation(listItem[a], listText[a], listIcon[a], listTitle[a], checkOut, a);
 }
 
